773237dff39e36df4b175c5a1cff871b22ac2c21
seba13680 <41175878+seba13680@users.noreply.github.com>
2019-03-06 14:13:29 -0300

    Add files via upload

---
+++ ConsoleAppCalculadoraUsandoSuma/ConsoleAppCalculadoraUsandoSuma/Operacion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCalculadoraUsandoSuma
+{
+    class Operacion
+    {
+            public double _Numero1;
+            public double _Numero2;
+            public double Resultado;
+            public double multiplicar;
+            public double dividir;
+
+        public double Numero1
+            {
+            get { return _Numero1; }
+            set { _Numero1 = value; }
+            }
+
+        public double Numero2
+            {
+            get { return _Numero2; }
+            set { _Numero2 = value; }
+            }
+
+        public double Sumar()
+        {
+        Resultado = Numero1 + Numero2;
+        return Resultado;
+        }
+
+        public double Restar()
+        {
+            var resultado = 0;
+            var resta = 0;
+            if (Numero1 >= Numero2)
+            {
+                for (int k = (int)Numero2; k < Numero1; k++)
+                { 
+                resta++;
+                }
+            }
+            else
+            {
+                for (int k = (int)Numero1; k < Numero2; k++)
+                { 
+                resta++;
+                }
+            }
+            resultado = resta;
+            return resultado;
+        }
+
+        public double Multiplicar()
+            {
+            for (int k = 0; k < Numero2; ++k)
+            {  
+                multiplicar += Numero1;
+            }
+            return multiplicar;
+        }
+
+        public double Dividir()
+            {
+            if (Numero1 >= Numero2)
+            {
+                for (int i = 0; i < (int)Numero1; i = i + (int)Numero2)
+                { 
+                    dividir++;
+                }
+            }
+            return dividir;
+            }
+    }
+}
---
+++ ConsoleAppCalculadoraUsandoSuma/ConsoleAppCalculadoraUsandoSuma/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCalculadoraUsandoSuma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seleccione una operación :\n1 - Sumar\n2 - Restar\n3 - Multiplicar\n4 - Dividir");
+
+            String opcion = Console.ReadLine();
+
+            if (opcion.Equals("1") || opcion.Equals("2") || opcion.Equals("3") || opcion.Equals("4"))
+            {
+                Operacion operacion = new Operacion();
+
+                Console.Write("Ingrese el número 1: ");
+                operacion.Numero1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el número 2: ");
+                operacion.Numero2 = int.Parse(Console.ReadLine());
+
+                if (opcion.Equals("1"))
+                {
+                    Console.WriteLine("El resultado es : " + operacion.Sumar());
+                }
+                if (opcion.Equals("2"))
+                {
+                    Console.WriteLine("El resultado es : " + operacion.Restar());
+                }
+                if (opcion.Equals("3"))
+                {
+                    Console.WriteLine("El resultado es : " + operacion.Multiplicar());
+                }
+                if (opcion.Equals("4"))
+                {
+                    Console.WriteLine("El resultado es : " + operacion.Dividir());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opción no válida");
+            }
+            Console.ReadKey();
+        }
+    }
+}
